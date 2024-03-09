3e55a708d3d11a4136710560198feff3d0627549
galina101 <goncharovagalinka@gmail.com>
2024-03-09 11:36:16 -0500

    saving lab progress: 03-09 11:36

--- src/components/parent-component/parent-component.tsx
+++ src/components/parent-component/parent-component.tsx
@@ -6,44 +6,39 @@ const ParentComponent: React.FC = () => {
     return (
         <React.Fragment>
             <h1>THE PARENT COMPONENT IS BEING DISPLAYED</h1>
-            <p>The concept of props in React allows a "parent" component to pass data to one or more "children" components. This allows children
-                components to be reused with different initial data, so they can display in different, yet similar, ways. The prop itself
-                takes the form of an object in TypeScript with various attributes that can be accessed then utilized in various way.
-                The attributes of a props can be customized to fit the needs of your application; we'll use interfaces to give structure to the props object.
-            </p>
+            <p>Paragraph 1</p>
 
-            <p>The way you create props differs depending on whether you're using a function component or a class component. In this lab, let's explore 
-                using props with both types of components. Take a look at the TSX file for each child component, you'll notice that there is a customized
-                interface that defines the datatypes of each props object.
-            </p>
+            <p>Paragraph 2</p>
 
-            <div className="section-container">
+            <div className="section-container1">
                 <h2>Using props with CLASS components.</h2>
 
                 <p>Here are two examples of props being passed to children CLASS components:</p>
 
-                {/* Curley braces in JSX/TSX allow you to access coding functionality that reduces into a singular values like accessing variable, processing ternary statements,
-                    calling functions to get a return value, etc. We can also use curly braces to produce comments in JSX/TSX like the one you're reading right now. */}
                 <ClassChildComponent myNumProp={7} myStringProp={"Class Child Data 1"}></ClassChildComponent>
                 <ClassChildComponent myNumProp={12} myStringProp={"Class Child Data 2"}></ClassChildComponent>
 
-                <div className="todo-container">
-                    <p>TODO: render the child CLASS component a third time with new values for the prop's attributes. Make the number 15 and the string "Class Child Data 25".</p>
-                    {/* CODE HERE */}
+                <div className="todo-container-class">
+                    <p>TODO: render the childCLASS component a third time with new values for the prop's attributes.
+                        Make the number 15 and the string "Class Child Data 25".</p>
+                    {<ClassChildComponent myNumProp={15} myStringProp={"Class Child Data 25"}></ClassChildComponent>}
+
                 </div>
             </div>
 
-            <div className="section-container bottom-gap">
+            <div className="section-container2 bottom-gap">
                 <h2>Using props with FUNCTION components.</h2>
 
-                <p>Here are two examples of props being passed to children FUNCTTION components:</p>
+                <p>Here are two examples of props being passed to children FUNCTION components:</p>
 
                 <FunctionChildComponent myNumProp={5} myStringProp={"Function Child Data 1"}></FunctionChildComponent>
                 <FunctionChildComponent myNumProp={10} myStringProp={"Function Child Data 2"}></FunctionChildComponent>
 
-                <div className="todo-container">
-                    <p>TODO: render the child FUNCTION component a third time with new values for the prop's attributes. Make the number 1 and the string "Function Child Data 33".</p>
-                    {/* CODE HERE */}
+                <div className="todo-container-class">
+                    <p>TODO: render the child FUNCTION component a third time with new values for the prop's attributes.
+                        Make the number 1 and the string "Function Child Data 33".</p>
+                    {<ClassChildComponent myNumProp={1} myStringProp={"Class Child Data 33"}></ClassChildComponent>}
+
                 </div>
             </div>
 
@@ -54,7 +49,8 @@ const ParentComponent: React.FC = () => {
         </React.Fragment>
     );
 
-    /* OPTIONAL PRACTICE: If you want more practice later, try passing other datatypes as props. In this example, we passed strings, numbers, and booleans; you should try passing an
+    /* OPTIONAL PRACTICE: If you want more practice later, try passing other datatypes as props.
+    In this example, we passed strings, numbers, and booleans; you should try passing an
         object and then try passing a function. */
 }
 
